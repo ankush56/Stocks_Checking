833c4268426192f132d3dd7a6a5fe65cb8299061
Aw <walia_77@outlook.com>
2022-07-06 21:33:45 -0400

    Added fetch news API functionality

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 load_dotenv()
 
 url = "https://www.alphavantage.co/query"
+NEWS_URL = "https://newsapi.org/v2/everything"
 
 API_KEY = os.environ.get('API_KEY')
 STOCK = "TSLA"
@@ -29,46 +30,63 @@ response = requests.get(url, parameters)
 response.raise_for_status()
 data = response.json()
 
-
 timeseries_daily = data['Time Series (Daily)']
-
-
 
 days_list = []
 
-#We dont know if there is previous day in weekday or holiday in between
+# We dont know if there is previous day in weekday or holiday in between
 # So we convert dict to iterator, so we can iterate with next
+
 iterator = iter(timeseries_daily.items())
 for i in range(2):
     a = next(iterator)
     days_list.append(a)
 
-
 # Trim tuples in list to get yesterday and day before yesterday max
 yesterday_max = float(days_list[0][1]['2. high'])
 day_before_yesterday_max = float(days_list[1][1]['2. high'])
 
-print(f"yesterday_max {yesterday_max}")
-print(f"day_before_yesterday_max {day_before_yesterday_max}")
+# print(f"yesterday_max {yesterday_max}")
+# print(f"day_before_yesterday_max {day_before_yesterday_max}")
 
 percentage_amount = (day_before_yesterday_max * PERCENTAGE_CHECK) / 100
-print(f"five_percentage {percentage_amount}")
+# print(f"five_percentage {percentage_amount}")
 
 difference = day_before_yesterday_max - yesterday_max
-print(f"difference {difference}")
+# print(f"difference {difference}")
 
 if yesterday_max > percentage_amount + day_before_yesterday_max:
     print(f"stock went up more than {PERCENTAGE_CHECK} % percentage")
 
 elif yesterday_max < day_before_yesterday_max - percentage_amount:
     print(f"Stock went down by {PERCENTAGE_CHECK} % percent")
+
 else:
     print(F"Stock neither went up by {PERCENTAGE_CHECK} %,  neither dropped")
 
 
+news_params = {
+    "q": COMPANY_NAME,
+    "sortBy": "publishedAt,relevancy",
+    "apiKey": os.environ.get('NEWS_API_KEY'),
+    "language": "en",
+}
 
 
+news_response = requests.get(NEWS_URL, news_params)
+news_response.raise_for_status()
+news_data = news_response.json()
 
+
+articles = news_data["articles"]
+articles_list = {}
+
+for x in range(0,4):
+    articles_list.update({f"Headline- {articles[x]['title']}": f"Brief- {articles[x]['description']}"})
+
+for x, y in articles_list.items():
+    print(x)
+    print(y)
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -76,11 +94,12 @@ else:
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
+
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number.
 
 
-#Optional: Format the SMS message like this:
+# Optional: Format the SMS message like this:
 # """
 # TSLA: 2%
 # Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
@@ -90,4 +109,3 @@ else:
 # Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
 # Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 # """
-
